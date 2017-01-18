3743d09e759e4da8a2d977dc774f8a2b8f631407
Mihaela_Iv <mihaela.ivanova.ivanova@gmail.com>
2017-01-18 22:02:13 +0200

    Arrays half homework added

---
+++ Homework/Arrays/increase-array-members.js
@@ -0,0 +1,10 @@
+function solve(args) {
+    var length =args[0];
+    var incresedArray=[];
+    for(var i=0; i<length; i+=1) {
+        incresedArray[i]=i*5;
+        console.log(incresedArray[i]); 
+        //console.log('\n'); 
+    }
+    
+}
